3b62ffe1437ae8407eb8c2e5ce6f760addd68729
Syafiza <syafiza.senin@pipeline-network.com>
2022-03-30 15:23:18 +0800

    blade-user

--- resources/views/pengurusan_pengguna/senarai_pengguna/ejen_pelaksana/index.blade.php
+++ resources/views/pengurusan_pengguna/senarai_pengguna/ejen_pelaksana/index.blade.php
@@ -36,6 +36,7 @@
                                     <th scope="col">E-MEL</th>
                                     <th scope="col">TARIKH AKAUN DICIPTA</th>
                                     <th scope="col">PENGAKTIFAN AKAUN</th>
+                                    <th scope="col">TINDAKAN</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -47,21 +48,36 @@
                                             <td>{{ $u->email }}</td>
                                             <td>{{ $u->created_at }}</td>
                                             <td>
-                                                <form action="/pengurusan_pengguna/pengguna/pengaktifan/{{$u->id}}" method="post" id="statusForm{{ $u->id }}" class="pengaktifan">
-                                                    @csrf
-                                                    <input class="form-check-input up-switch-risda" id="active_{{ $u->id }}"
-                                                    type="checkbox" value="1"
-                                                    {{ isset($u->status_akaun) && $u->status_akaun == '1' ? 'checked' : '' }}
-                                                    name="status"
-                                                    onchange="document.getElementById('statusForm{{ $u->id }}').submit()" />
-                                                </form>
+                                                <div class="form-switch">
+
+                                                    <form action="/pengurusan_pengguna/pengguna/pengaktifan/{{$u->id}}" method="post" id="statusForm{{ $u->id }}" class="pengaktifan">
+                                                        @csrf
+                                                        <input class="form-check-input up-switch-risda" id="active_{{ $u->id }}"
+                                                        type="checkbox" value="1"
+                                                        {{ isset($u->status_akaun) && $u->status_akaun == '1' ? 'checked' : '' }}
+                                                        name="status"
+                                                        onchange="document.getElementById('statusForm{{ $u->id }}').submit()" />
+                                                    </form>
+                                                </div>
                                                 {{-- <button class="btn btn-primary" type="button"
                                                     data-bs-toggle="modal"
                                                     data-bs-target="#edit_pengguna_{{ $u->id }}">
                                                     <i class="fas fa-pen"></i>
                                                 </button> --}}
                                             </td>
+
+                                            <td>
+                                                <button class="btn btn-primary" type="button" data-bs-toggle="modal"
+                                                data-bs-target="#edit_pengguna_{{ $u->id }}"><i class="fas fa-pen"></i>
+                                                </button>
+
+                                                <button class="btn risda-bg-dg text-white" type="button" data-bs-toggle="modal"
+                                                    data-bs-target="#delete_{{ $u->id }}"><i class="fas fa-trash"></i>
+                                                </button>
+                                            </td>
                                         </tr>
+
+
                                     <div class="modal fade" id="edit_pengguna_{{ $u->id }}"
                                         tabindex="-1" role="dialog" aria-hidden="true">
                                         <div class="modal-dialog modal-dialog-centered" role="document"
@@ -114,13 +130,55 @@
                                             </div>
                                         </div>
                                     </div>
+
+                                    <div class="modal fade" id="delete_{{ $u->id }}" tabindex="-1"
+                                        role="dialog" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document"
+                                            style="max-width: 500px">
+                                            <div class="modal-content position-relative">
+                                                <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+                                                    <button
+                                                        class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
+                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body p-0">
+                                                    <div class="row">
+                                                        <div class="col text-center m-3">
+                                                            <i class="far fa-times-circle fa-7x" style="color: #ea0606"></i>
+                                                            <br>
+                                                            Anda pasti untuk menghapus data {{$u->nama}} ini?
+
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button class="btn btn-secondary" type="button"
+                                                            data-bs-dismiss="modal">Batal</button>
+                                                        <form method="POST" action="/pengurusan_pengguna/pengguna/staf/{{$u->id}}">
+                                                            @method('DELETE')
+                                                            @csrf
+                                                            <button class="btn btn-primary" type="submit">Hapus
+                                                            </button>
+                                                        </form>
+
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+
                                 @endforeach
-                            </tbody>
-                        </table>
+                           </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
     </div>
+
+
 @endsection
