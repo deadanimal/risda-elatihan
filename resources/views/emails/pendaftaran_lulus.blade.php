213e29a0f8d2a325a2995fbefc448dbb6164ab55
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-03-30 18:26:04 +0800

    debug

--- resources/views/emails/pendaftaran_lulus.blade.php
+++ resources/views/emails/pendaftaran_lulus.blade.php
@@ -2,7 +2,7 @@
 
 <p class="mt-4">Tuan/Puan,</p>
 
-<p class="mt-4">Permohonan anda bagi {{$permohonan->jadualKursus->kursus_nama}} telah diluluskan.</p>
+<p class="mt-4">Permohonan anda bagi {{$permohonan->jadual->kursus_nama}} telah diluluskan.</p>
 
 <p class="mt-4">Sekian, terima kasih.</p>
 
