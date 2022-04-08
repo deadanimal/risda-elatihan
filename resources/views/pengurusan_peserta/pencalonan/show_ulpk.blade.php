24b88e5b159a591054211a697ff51a01602f9101
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-04-08 11:43:22 +0800

    CL - combined Permohonan and Pencalonan view

--- resources/views/pengurusan_peserta/pencalonan/show_ulpk.blade.php
+++ resources/views/pengurusan_peserta/pencalonan/show_ulpk.blade.php
@@ -32,8 +32,7 @@
                                 <p class="h5">Staf</p>
                             </div>
                             <div class="col-lg-6">
-                                <input type="text" class="form-control form-control-sm mb-2" disabled
-                                    value="Pekebun Kecil">
+                                <input type="text" class="form-control form-control-sm mb-2" disabled value="Pekebun Kecil">
                             </div>
                             <div class="col-lg-6">
                                 <p class="h5">Kod Nama Kursus</p>
@@ -80,53 +79,72 @@
                         @foreach ($peserta_daftar as $key => $p)
                             <tr>
                                 <td>{{ $key + 1 }}.</td>
-                                <td>{{ $p->maklumat_peserta->no_KP }}</td>
-                                <td>{{ $p->maklumat_peserta->name }}</td>
+                                <td>{{ $p->peserta->no_KP }}</td>
+                                <td>{{ $p->peserta->name }}</td>
                                 <td>{{ $p->alamat }}</td>
-                                <td>{{ $p->status }}</td>
                                 <td>
-                                    <button class="btn btn-sm btn-primary" type="button" data-bs-toggle="modal"
-                                        data-bs-target="#delete-{{ $p->id }}">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
+                                    @if ($p->status_permohonan == 0)
+                                        Belum Disemak
+                                    @elseif($p->status_permohonan == 1)
+                                        Belum Disemak (Sokongan)
+                                    @elseif($p->status_permohonan == 2)
+                                        Disokong
+                                    @elseif($p->status_permohonan == 3)
+                                        Tidak Disokong
+                                    @elseif($p->status_permohonan == 4)
+                                        Diluluskan
+                                    @elseif($p->status_permohonan == 5)
+                                        Tidak Lulus
+                                    @elseif($p->status_permohonan == 6)
+                                        Dicalonkan
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($p->status_permohonan == 6)
+                                        <button class="btn btn-sm btn-primary" type="button" data-bs-toggle="modal"
+                                            data-bs-target="#delete-{{ $p->id }}">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    @endif
+
                                 </td>
                             </tr>
 
-                            <div class="modal fade" id="delete-{{ $p->id }}" tabindex="-1"
-                                role="dialog" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered" role="document"
-                                        style="max-width: 500px">
-                                        <div class="modal-content position-relative">
-                                            <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-                                                <button
-                                                    class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
-                                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body p-0">
-                                                <div class="row">
-                                                    <div class="col text-center m-3">
-                                                        <i class="far fa-times-circle fa-7x" style="color: #ea0606"></i>
-                                                        <br>
-                                                        Anda pasti untuk menghapus pencalonan ini?
-
-                                                    </div>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button class="btn btn-secondary" type="button"
-                                                        data-bs-dismiss="modal">Batal</button>
-                                                    <form method="POST" action="/pengurusan_peserta/pencalonan/{{$p->id}}">
-                                                        @method('DELETE')
-                                                        @csrf
-                                                        <button class="btn btn-primary" type="submit">
-                                                            Hapus
-                                                        </button>
-                                                    </form>
-
-                                                </div>
-                                            </div>
-
+                            <div class="modal fade" id="delete-{{ $p->id }}" tabindex="-1" role="dialog"
+                                aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 500px">
+                                    <div class="modal-content position-relative">
+                                        <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+                                            <button
+                                                class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base"
+                                                data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
+                                        <div class="modal-body p-0">
+                                            <div class="row">
+                                                <div class="col text-center m-3">
+                                                    <i class="far fa-times-circle fa-7x" style="color: #ea0606"></i>
+                                                    <br>
+                                                    Anda pasti untuk menghapus pencalonan ini?
+
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button class="btn btn-secondary" type="button"
+                                                    data-bs-dismiss="modal">Batal</button>
+                                                <form method="POST"
+                                                    action="/pengurusan_peserta/pencalonan/{{ $p->id }}">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button class="btn btn-primary" type="submit">
+                                                        Hapus
+                                                    </button>
+                                                </form>
+
+                                            </div>
+                                        </div>
+
                                     </div>
+                                </div>
                             </div>
                         @endforeach
                     </tbody>
