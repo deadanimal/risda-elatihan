492d264eb9180a7921b7dab010a0b989a1a9bd0b
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-05-26 18:38:26 +0800

    VG - Bugs fixing 1 Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-3">
+        <div class="row mt-3 justify-content-center">
             <div class="col-lg-4 mb-2">
                 <div class="card" style="background-color:#0F5E31;">
                     <div class="card-body">
@@ -47,34 +47,6 @@
                             </div>
                             <div class="col-5 text-center">
                                 <h3 class="text-white mb-0">{{ $bulan_depan }}</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-2">
-                <div class="card" style="background-color:#0F5E31;">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-7 text-center">
-                                <h3 class="h5 text-white mb-0">PEMANTAUAN PESERTA</h3>
-                            </div>
-                            <div class="col-5 text-center">
-                                <h3 class="text-white mb-0">0</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 mb-2">
-                <div class="card" style="background-color:#0F5E31;">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-7 text-center">
-                                <h3 class="h5 text-white mb-0">KURSUS LEWAT JADUAL</h3>
-                            </div>
-                            <div class="col-5 text-center">
-                                <h3 class="text-white mb-0">0</h3>
                             </div>
                         </div>
                     </div>
