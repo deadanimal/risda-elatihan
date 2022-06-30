7958d91a1355cd9c841ba593b7a2be38f5c71dbb
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-06-30 12:54:38 +0800

    DEBUG on environment server

--- resources/views/uls/urus_setia/pengajian_lanjutan/index.blade.php
+++ resources/views/uls/urus_setia/pengajian_lanjutan/index.blade.php
@@ -146,10 +146,10 @@
                                 @foreach ($pengajian_lanjutan as $pl)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $pl->pengguna->no_KP }}</td>
-                                        <td>{{ $pl->pengguna->name }}</td>
-                                        <td>{{ $pl->data_staf->Jawatan }}</td>
-                                        <td>{{ $pl->data_staf->Gred }}</td>
+                                        <td>{{ ($pl->pengguna->no_KP ?? '') }}</td>
+                                        <td>{{ ($pl->pengguna->name ?? '') }}</td>
+                                        <td>{{ ($pl->data_staf->Jawatan ?? '') }}</td>
+                                        <td>{{ ($pl->data_staf->Gred ?? '') }}</td>
                                         <td>{{ ($pl->ipt['nama_Agensi'] ?? '') }}</td>
                                         <td>{{ $pl->bidang_pengajian }}</td>
                                         <td>{{ date('d-m-Y', strtotime($pl->tarikh_mula_pengajian)) }}</td>
