edf6da8cf2340f5b050fbd0c4516f65fe026075e
njhn-pnsb <najhan.najib@pipeline-network.com>
2022-07-13 14:51:21 +0800

    CL - fix add PK (based on API)

--- app/Http/Controllers/SemakanController.php
+++ app/Http/Controllers/SemakanController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Mail\PendaftaranEP;
 use App\Mail\PendaftaranPK;
+use App\Models\GAP;
 use App\Models\PekebunKecil;
 use App\Models\Staf;
 use App\Models\Tanah;
@@ -128,6 +129,115 @@ class SemakanController extends Controller
             $user->status_akaun = '1';
 
             $user->save();
+
+            // save data pekebun kecil from api
+            $data_pk = Http::withBasicAuth('99891c082ecccfe91d99a59845095f9c47c4d14e', '1cc11a9fec81dc1f99f353f403d6f5bac620aa8f')
+                ->get('https://www4.risda.gov.my/espek/portalpkprofiltanah/?nokp=' . $request->no_KP)
+                ->getBody()
+                ->getContents();
+
+            $data_pk = json_decode($data_pk, true);
+            $data = $data_pk[0];
+
+            $pk = new PekebunKecil;
+            $pk->id_Pengguna = $user->id;
+            $pk->Jantina_ID = $data['Jantina_ID'];
+            $pk->Jantina = $data['Jantina'];
+            $pk->Warganegara_ID = $data['Warganegara_ID'];
+            $pk->Warganegara = $data['Warganegara'];
+            $pk->Bangsa_ID = $data['U_Bangsa_ID'];
+            $pk->Bangsa = $data['Bangsa'];
+            $pk->Nombor = $data['Nombor'];
+            $pk->Jalan = $data['Jalan'];
+            $pk->Nama_Kampung = $data['Nama_Kampung'];
+            $pk->Bandar = $data['Bandar'];
+            $pk->Poskod = $data['Poskod'];
+            $pk->U_Negeri_ID = $data['U_Negeri_ID'];
+            $pk->Negeri = $data['Negeri'];
+            $pk->U_Daerah_ID = $data['U_Daerah_ID'];
+            $pk->Daerah = $data['Daerah'];
+            $pk->U_Mukim_ID = $data['U_Mukim_ID'];
+            $pk->Mukim = $data['Mukim'];
+            $pk->U_Kampung_ID = $data['U_Kampung_ID'];
+            $pk->Kampung = $data['Kampung'];
+            $pk->U_Seksyen_ID = $data['U_Seksyen_ID'];
+            $pk->Seksyen = $data['Seksyen'];
+            $pk->Penempatan_ID = $data['Penempatan_ID'];
+            $pk->Penempatan = $data['Penempatan'];
+            $pk->Telefon = $data['Telefon'];
+            $pk->U_Parlimen_ID = $data['U_Parlimen_ID'];
+            $pk->Parlimen = $data['Parlimen'];
+            $pk->U_Dun_ID = $data['U_Dun_ID'];
+            $pk->Dun = $data['Dun'];
+            $pk->Kod_PT_Lokaliti = $data['Kod_PT_Lokaliti'];
+            $pk->Pusat_Tanggungjawab_Lokaliti = $data['Pusat_Tanggungjawab_Lokaliti'];
+
+            $pk->save();
+
+            // save data pekebun kecil from api
+            foreach ($data['Tanah'] as $key => $dtanah) {
+                $tanah = new Tanah;
+                $tanah->id_pekebun_kecil = $pk->id;
+                $tanah->U_Tanah_ID = $dtanah['U_Tanah_ID'];
+                $tanah->No_Geran = $dtanah['No_Geran'];
+                $tanah->No_Lot = $dtanah['No_Lot'];
+                $tanah->U_SyaratT_ID = $dtanah['U_SyaratT_ID'];
+                $tanah->Syarat = $dtanah['Syarat'];
+                $tanah->U_Negeri_ID = $dtanah['U_Negeri_ID'];
+                $tanah->U_Daerah_ID = $dtanah['U_Daerah_ID'];
+                $tanah->U_Mukim_ID = $dtanah['U_Mukim_ID'];
+                $tanah->U_Seksyen_ID = $dtanah['U_Seksyen_ID'];
+                $tanah->U_Kampung_ID = $dtanah['U_Kampung_ID'];
+                $tanah->U_Parlimen_ID = $dtanah['U_Parlimen_ID'];
+                $tanah->U_Dun_ID = $dtanah['U_Dun_ID'];
+                $tanah->Luas_Lot = $dtanah['Luas_Lot'];
+                $tanah->Bahagian_Pemilikan = $dtanah['Bahagian_Pemilikan'];
+                $tanah->Luas_Pemilikan = $dtanah['Luas_Pemilikan'];
+                $tanah->Bil_Pemilik_Atas_Geran = $dtanah['Bil_Pemilik_Atas_Geran'];
+                $tanah->U_Pengurusan_Tanah_ID = $dtanah['U_Pengurusan_Tanah_ID'];
+                $tanah->Pengurusan = $dtanah['Pengurusan'];
+                $tanah->U_Taraf_ID = $dtanah['U_Taraf_ID'];
+                $tanah->Taraf = $dtanah['Taraf'];
+                $tanah->Penempatan_id = $dtanah['Penempatan_id'];
+                $tanah->Penempatan = $dtanah['Penempatan'];
+                $tanah->Kod_PT_Tanah = $dtanah['Kod_PT_Tanah'];
+                $tanah->Pusat_Tanggungjawab_Tanah = $dtanah['Pusat_Tanggungjawab_Tanah'];
+
+                $tanah->save();
+
+                foreach ($dtanah['Tanaman'] as $key => $dtanaman) {
+                    $tanaman = new Tanaman;
+                    $tanaman->id_tanah = $tanah->id;
+                    $tanaman->Jenis_Tanaman = $dtanaman['Jenis_Tanaman'];
+                    $tanaman->Peratus_Tanaman = $dtanaman['Peratus_Tanaman'];
+                    $tanaman->Luas_Tanaman = $dtanaman['Luas_Tanaman'];
+                    $tanaman->U_Lib_Jenis_Tanaman = $dtanaman['U_Lib_Jenis_Tanaman_ID'];
+                    $tanaman->Tahun_Tanam = $dtanaman['Tahun_Tanam'];
+                    $tanaman->U_Agensi_Bantuan_ID = $dtanaman['U_Agensi_Bantuan_ID'];
+                    $tanaman->Agensi = $dtanaman['Agensi'];
+                    $tanaman->U_Pendekatan_ID = $dtanaman['U_Pendekatan_ID'];
+                    $tanaman->Pendekatan = $dtanaman['Pendekatan'];
+                    $tanaman->Hasil = $dtanaman['Hasil'];
+                    $tanaman->U_JProduk_ID = $dtanaman['U_JProduk_ID'];
+                    $tanaman->Produk = $dtanaman['Produk'];
+                    $tanaman->U_SPengusahan_ID = $dtanaman['U_SPengusahan_ID'];
+                    $tanaman->Status = $dtanaman['Status'];
+
+                    $tanaman->save();
+
+                    foreach ($dtanaman['GAP'] as $key => $dgap) {
+                        $gap = new GAP;
+                        $gap->id_tanaman=$tanaman->id;
+                        $gap->U_JnsAmalan_ID=$dgap['U_JnsAmalan_ID'];
+                        $gap->Amalan=$dgap['Amalan'];
+                        $gap->U_Tahap_ID=$dgap['U_Tahap_ID'];
+                        $gap->Tahap=$dgap['Tahap'];
+
+                        $gap->save();
+                    }
+                }
+            }
+
             Mail::to($request->email)->send(new PendaftaranPK($user));
             alert()->success('Sila semak email anda untuk notifikasi pendaftaran.', 'Pendaftaran Berjaya');
             return redirect('/');
